51bee08fe535a00312598acadb8bad750f6dde29
vishal lokhande <lvishal108@gmail.com>
2025-09-03 13:21:27 +0000

    jinja templates and config URL

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -1,45 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tweet
 from .forms import TweetForm
-from django .shortcuts import get_object_or_404 , redirect
-# Create your views here.
 
+# Home Page
 def index(request):
     return render(request, 'index.html')
 
 
+# List Tweets
 def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
+
+# Create Tweet
 def tweet_create(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
         if form.is_valid():
-            tweet = form.save(commit = False)
+            tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
             return redirect('tweet_list')
     else:
         form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
 
 
+# Edit Tweet
 def tweet_edit(request, tweet_id):
-    tweet = get_object_or_404(Tweet, id=tweet_id , user = request.FILES, instance=tweet)
+    tweet = get_object_or_404(Tweet, id=tweet_id, user=request.user)
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES, instance=tweet)
         if form.is_valid():
-            tweet = form.save(commit = False)
-            tweet.user = request.user
-            tweet.save()
+            form.save()
             return redirect('tweet_list')
     else:
         form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form': form})
 
 
+# Delete Tweet
 def tweet_delete(request, tweet_id):
-    tweet = get_object_or_404(Tweet, id=tweet_id , user=request.user)
+    tweet = get_object_or_404(Tweet, id=tweet_id, user=request.user)
     if request.method == 'POST':
         tweet.delete()
         return redirect('tweet_list')
